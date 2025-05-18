5e3193ff89c09628ef41dd37bdb9e6e1bbd0a4ac
PloBuk-l <plobuk5@gmail.com>
2025-05-18 10:56:45 +0800

    1.1
    Textures

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from pygame import *
-from random import randint
+from random import randint as geforg
 from time import time as timer
 
 
+
 w_w, w_h = 700, 500
+
+font.init()
+
+font1 = font.Font(None, 50)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+font2 = font.Font(None, 50)
+lose2 = font2.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
 win = display.set_mode((w_w, w_h))
 
 display.set_caption('Пинг-понг')
 
-background = transform.scale(image.load('background.jpg'), (w_w, w_h))
+background = transform.scale(image.load('backgroundd.png'), (w_w, w_h))
+losee = transform.scale(image.load('losee.png'), (w_w, w_h))
 
 clock = time.Clock()
 FPS = 60
@@ -48,9 +57,13 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < 400:
             self.rect.y += self.speed
 
-player1 = Player('./Rocket.jpg', 20, 20, 5, 32, 100)
-player2 = Player('./Rocket.jpg', 648, 400, 5, 32, 100)
+r, l = geforg(100, 600), geforg(50, 400)
 
+player1 = Player('./Rocket1.png', 20, 20, 5, 32, 100)
+player2 = Player('./Rocket1.png', 648, 400, 5, 32, 100)
+ball = Player('./pngg.png', r, l, 3, 50, 50)
+
+speed_x, speed_y = 3, 3
 finish = False
 game = True
 
@@ -60,11 +73,30 @@ while game:
             game = False
 
     if not finish:
-        win.blit(background,(0, 0 ))
+        win.blit(background,(0, 0))
         player1.reset()
         player1.update_l()
         player2.reset()
         player2.update_r()
+        ball.update()
+        ball.reset()
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > w_h - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+            speed_x *= -1
+        if ball.rect.x < 0:
+            finish = True
+            losee = transform.scale(image.load('losee.png'), (w_w, w_h))
+            win.blit(losee,(0, 0))
+            win.blit(lose1, (200, 200))
+        if ball.rect.x > 700:
+            finish = True
+            losee = transform.scale(image.load('losee.png'), (w_w, w_h))
+            win.blit(losee,(0, 0))
+            win.blit(lose2, (200, 200))
 
     display.update()
     clock.tick(FPS)
